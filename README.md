af1a91c2e5e91b04f4dfe7b96905343df7a5a559
willrsq1 <zeldacartoon@gmail.com>
2024-06-12 21:44:11 +0200

    commit test to clone back and see if it works as described in the readme AGAIN because added a script that does it automatically

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 Requirements:
 
+	- cmake
+	- internet connection to clone the libcpr Library (https://docs.libcpr.org/)
 	- a C++17 compatible compiler such as Clang or GCC. The minimum required version of GCC is unknown
 
 How to use:
 
-	- cb build && cmake .. && make && ./invi_a
+	- run `sh launch.sh`
+		or manually:
+	- run `mkdir build && cd build && cmake .. && make && ./invi_a`
--- build/_deps/cpr-subbuild/CMakeFiles/Makefile.cmake
+++ build/_deps/cpr-subbuild/CMakeFiles/Makefile.cmake
@@ -10,8 +10,10 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.22.1/CMakeSystem.cmake"
   "CMakeLists.txt"
   "cpr-populate-prefix/tmp/cpr-populate-cfgcmd.txt.in"
+  "/usr/share/cmake-3.22/Modules/CMakeDetermineSystem.cmake"
   "/usr/share/cmake-3.22/Modules/CMakeGenericSystem.cmake"
   "/usr/share/cmake-3.22/Modules/CMakeInitializeConfigs.cmake"
+  "/usr/share/cmake-3.22/Modules/CMakeSystem.cmake.in"
   "/usr/share/cmake-3.22/Modules/CMakeSystemSpecificInformation.cmake"
   "/usr/share/cmake-3.22/Modules/CMakeSystemSpecificInitialize.cmake"
   "/usr/share/cmake-3.22/Modules/ExternalProject-gitupdate.cmake.in"
@@ -29,6 +31,7 @@ set(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
+  "CMakeFiles/3.22.1/CMakeSystem.cmake"
   "cpr-populate-prefix/src/cpr-populate-stamp/cpr-populate-gitinfo.txt"
   "cpr-populate-prefix/tmp/cpr-populate-gitupdate.cmake"
   "cpr-populate-prefix/tmp/cpr-populate-cfgcmd.txt"
